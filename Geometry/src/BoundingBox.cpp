ceb0fe908fb90824cfeed3c11b88a20d6a009045
jaydeep-jogdand <jaydeep.jogdand@cctech.co.in>
2024-11-25 11:50:14 +0530

    Bounding box Update

--- Geometry/src/BoundingBox.cpp
+++ Geometry/src/BoundingBox.cpp
@@ -15,31 +15,15 @@ BoundingBox::~BoundingBox()
 
 std::vector<SurfacePoint> BoundingBox::getBounds()
 {
-    SurfacePoint TBL = SurfacePoint(lowestBound.X(), highestBound.Y(), lowestBound.Z());
-    SurfacePoint TBR = SurfacePoint(highestBound.X(), highestBound.Y(), lowestBound.Z());
-    SurfacePoint TFL = SurfacePoint(lowestBound.X(), highestBound.Y(), highestBound.Z());
-    SurfacePoint TFR = SurfacePoint(highestBound.X(), highestBound.Y(), highestBound.Z());
-    SurfacePoint BBL = SurfacePoint(lowestBound.X(), lowestBound.Y(), lowestBound.Z());
-    SurfacePoint BBR = SurfacePoint(highestBound.X(), lowestBound.Y(), lowestBound.Z());
-    SurfacePoint BFL = SurfacePoint(lowestBound.X(), lowestBound.Y(), highestBound.Z());
-    SurfacePoint BFR = SurfacePoint(highestBound.X(), lowestBound.Y(), highestBound.Z());
+    SurfacePoint topBackLeftPoint(lowestBound.X(), highestBound.Y(), lowestBound.Z());
+    SurfacePoint topBackRightPoint(highestBound.X(), highestBound.Y(), lowestBound.Z());
+    SurfacePoint topFrontLeftPoint(lowestBound.X(), highestBound.Y(), highestBound.Z());
+    SurfacePoint topFrontRightPoint(highestBound.X(), highestBound.Y(), highestBound.Z());
+    SurfacePoint bottomBackLeftPoint(lowestBound.X(), lowestBound.Y(), lowestBound.Z());
+    SurfacePoint bottomBackRightPoint(highestBound.X(), lowestBound.Y(), lowestBound.Z());
+    SurfacePoint bottomFrontLeftPoint(lowestBound.X(), lowestBound.Y(), highestBound.Z());
+    SurfacePoint bottomFrontRightPoint(highestBound.X(), lowestBound.Y(), highestBound.Z());
+    std::vector<SurfacePoint> bounds = { topBackLeftPoint, topBackRightPoint, topFrontLeftPoint, topFrontRightPoint, bottomBackLeftPoint, bottomBackRightPoint, bottomFrontLeftPoint, bottomFrontRightPoint };
 
-    std::vector<SurfacePoint> bounds = { TBL, TBR, TFL, TFR, BBL, BBR, BFL, BFR };
     return bounds;
 }
-
-//std::vector<std::vector<double>> BoundingBox::generateVertices() const {
-//    std::vector<std::vector<double>> boundingBox;
-//    boundingBox.reserve(8); // Reserve space for 8 vertices
-//
-//    boundingBox.emplace_back(xMin, yMin, zMin);
-//    boundingBox.emplace_back(xMin, yMin, zMax);
-//    boundingBox.emplace_back(xMin, yMax, zMin);
-//    boundingBox.emplace_back(xMin, yMax, zMax);
-//    boundingBox.emplace_back(xMax, yMin, zMin);
-//    boundingBox.emplace_back(xMax, yMin, zMax);
-//    boundingBox.emplace_back(xMax, yMax, zMin);
-//    boundingBox.emplace_back(xMax, yMax, zMax);
-//
-//    return boundingBox;
-//}
